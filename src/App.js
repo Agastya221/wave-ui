c451ccaf1ccd1266608d58a1af2a361fc98f7d51
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-10-25 10:54:08 +0300

    🎉 Init Repo

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        <span role="img" aria-label="wave">👋</span> Hey there!
+        </div>
+
+        <div className="bio">
+        I'm <b>Greg</b> and I worked only on <b>Web2</b> websites so getting to <b>Web3</b> is really entertaining right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
