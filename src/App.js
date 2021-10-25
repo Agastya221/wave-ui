cf3f728122edeb780be8a61887bd399a4dedec72
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-10-25 17:57:18 +0300

    🚀Finished the dApp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React,{useEffect,useState} from "react";
 import { ethers } from "ethers";
-import abi from './utils/WavePortal.json';
 import HashLoader from "react-spinners/HashLoader";
 import {FiSend} from "react-icons/fi";
+import {contractAddress,contractABI} from "./constants";
 import LocalizedFormat from "dayjs/plugin/localizedFormat";
 import './App.scss';
 import dayjs from "dayjs";
@@ -13,10 +13,7 @@ export default function App() {
   const [allWaves, setAllWaves] = useState([]);
   const [loading,setLoading]=useState(false);
   const [wavesCount,setWavesCount]=useState("??");
-  // ETH Contract const
-  const contractAddress="0xAaB513Cb3Aa8cb062D5eb43Ccf52A02CA900cE97";
-  const contractABI = abi.abi;
-  // End ETH Contract const
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
@@ -24,10 +21,7 @@ export default function App() {
       if (!ethereum) {
         console.log("Make sure you have metamask!");
         return;
-      } else {
-        console.log("We have the ethereum object", ethereum);
       }
-      
       /*
       * Check if we're authorized to access the user's wallet
       */
@@ -35,7 +29,7 @@ export default function App() {
       
       if (accounts.length !== 0) {
         const account = accounts[0];
-        console.log("Found an authorized account:", account);
+        //console.log("Found an authorized account:", account);
         setCurrentAccount(account)
       } else {
         console.log("No authorized account found")
@@ -64,15 +58,11 @@ export default function App() {
   }
   useEffect(() => {
     checkIfWalletIsConnected();
-    
-  }, []);
-  useEffect(()=> {
-    const interval = setInterval(() => {
-            retrieveCount();
-            getAllWaves();
-        }, 1000);
-    return () => clearInterval(interval);
   });
+  useEffect(()=>{
+    retrieveCount();
+    getAllWaves();
+  })
   const retrieveCount=async()=> {
     const {ethereum}=window;
     if (ethereum) {
@@ -81,7 +71,13 @@ export default function App() {
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
         let count = await wavePortalContract.getTotalWaves();
         setWavesCount(count.toNumber());
-       
+       /**
+         * Listen in for emitter events!
+         */
+        wavePortalContract.on("NewWave", async() => {
+          let count = await wavePortalContract.getTotalWaves();
+          setWavesCount(count.toNumber());
+        });
     }
   }
   const wave = async () => {
@@ -100,7 +96,7 @@ export default function App() {
         /*
         * Execute the actual wave from your smart contract
         */
-        const waveTxn = await wavePortalContract.wave(message);
+        const waveTxn = await wavePortalContract.wave(message,{ gasLimit: 300000 });
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
@@ -141,7 +137,8 @@ export default function App() {
           wavesCleaned.push({
             address: wave.waver,
             timestamp: new Date(wave.timestamp * 1000),
-            message: wave.message
+            message: wave.message,
+            winner:wave.winner
           });
         });
 
@@ -149,6 +146,20 @@ export default function App() {
          * Store our data in React State
          */
         setAllWaves(wavesCleaned);
+
+        /**
+         * Listen in for emitter events!
+         */
+         wavePortalContract.on("NewWave", (from, timestamp, message,winner) => {
+          const newWave={address: from,
+            timestamp: new Date(timestamp * 1000),
+            message: message,
+            winner:winner};
+            if (allWaves.find((el)=>newWave===el)) {
+              console.log(allWaves.find((el)=>newWave.timestamp===el.timestamp));
+              setAllWaves(prevState => [...prevState, newWave]);
+            }
+        });
       } else {
         console.log("Ethereum object doesn't exist!")
       }
@@ -161,13 +172,14 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-        <span role="img" aria-label="wave">👋</span> Hey there!
+        <span role="img" aria-label="wave">👋</span> Hello there!
         </div>
 
         <div className="bio">
-        I'm <b>Greg</b> and I worked only on <b>Web2</b> websites so getting to <b>Web3</b> is really entertaining right? Connect your Ethereum wallet and wave at me!
+        I'm <b>Greg</b> and I've worked only on <b>Web2</b> websites up until now so getting to <b>Web3</b> seems really <span role="img" aria-label="cool">❄️</span>,right?
+         <br/>Connect your Ethereum wallet and try waving at me! Maybe you get to earn some <b>Ξ</b>       
         </div>
-        <div className="card">
+        {currentAccount&&<div className="card">
           <div className="card-inner">
             <label>Send a Wave </label>
             <div className="container">
@@ -176,7 +188,7 @@ export default function App() {
                 onChange={e=>setMessage(e.target.value)}
                 placeholder="The taughts you want to share"/>
               </div>
-              <button className="send" onClick={wave}>
+              <button className="send" onClick={wave} disabled={loading}>
           {loading
           ?<HashLoader size={20}/>
           :<FiSend/>
@@ -184,27 +196,26 @@ export default function App() {
         </button>
             </div>
           </div>
-        </div>
+        </div>}
         
         {!currentAccount && (
           <button className="waveButton" onClick={connectWallet}>
             Connect Wallet
           </button>
         )}
-        <div className="totalScore">
+        {currentAccount&&<div className="totalScore">
           Total Waves: {wavesCount}
-        </div>
+        </div>}
         {allWaves.map((wave, index) => {
-          const timeStamp =new Date(wave.timestamp.toString());
-
+          
           return (
             <div key={index} className="card">
               <div className="card-inner">
               <div>
                 <span className="addy">
-                  {wave.address}
-                </span>
-                <p>Posted on {dayjs(timeStamp).format('LLL')}</p>
+                  {wave.address} 
+                </span>{wave.winner?'🏆':""}
+                <p>Posted on {dayjs(wave.timestamp).format('LLL')}</p>
                 <p style={{color:"#ffb86c"}}>{wave.message}</p>
                 </div>
               </div>
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+import abi from './utils/WavePortal.json';
+export const contractABI=abi.abi;
+export const contractAddress="0x9a704228031F0561DBd07E29151C348691f15a31";
